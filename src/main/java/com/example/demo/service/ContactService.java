d2efedffde92ba06a7e87aed5f56f4481f2e8a71
Sateesh <naidusatish737@gmail.com>
2026-03-31 21:58:59 +0530

    Fixed ContactController method

--- src/main/java/com/example/demo/service/ContactService.java
+++ src/main/java/com/example/demo/service/ContactService.java
@@ -1,8 +1,6 @@
 package com.example.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Contact;
@@ -14,27 +12,45 @@ public class ContactService {
     @Autowired
     private ContactRepository contactRepository;
 
-    @Autowired
-    private JavaMailSender mailSender;
+    // Save contact to database
+    public void saveContact(Contact contact) {
 
-    public void saveAndSendEmail(Contact contact) {
-
-        // Save to database
         contactRepository.save(contact);
 
-        // Send email
-        SimpleMailMessage message = new SimpleMailMessage();
+        System.out.println("Contact saved successfully");
 
-        message.setTo("yourgmail@gmail.com"); // YOUR EMAIL
-
-        message.setSubject("New Contact Message");
-
-        message.setText(
-                "Name: " + contact.getName() +
-                "\nEmail: " + contact.getEmail() +
-                "\nMessage: " + contact.getMessage()
-        );
-
-        mailSender.send(message);
     }
+
+	public void saveAndSendEmail(Contact contact) {
+		
+		
+	}
 }
+
+
+
+
+
+//package com.example.demo.service;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import com.example.demo.entity.Contact;
+//import com.example.demo.repository.ContactRepository;
+//
+//@Service
+//public class ContactService {
+//
+//    @Autowired
+//    private ContactRepository contactRepository;
+//
+//    // Save contact to database
+//    public void saveContact(Contact contact) {
+//
+//        contactRepository.save(contact);
+//
+//        System.out.println("Contact saved successfully");
+//
+//    }
+//}
