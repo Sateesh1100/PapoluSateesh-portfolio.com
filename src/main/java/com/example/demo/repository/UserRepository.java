534f01c44075c89e6c3c0631ef79e13e61eeda68
Sateesh <naidusatish737@gmail.com>
2026-03-31 20:59:33 +0530

    Changed Java version to 17

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,8 +1,20 @@
+//package com.example.demo.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import com.example.demo.entity.User;
+//
+//public interface UserRepository extends JpaRepository<User, Integer> {
+//
+//}
+
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.example.demo.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
